74ca618941cda7118da6100186b3a818918a0930
lovac42 <lovac42@protonmail.com>
2020-02-16 00:20:22 -0500

    Updated copyright year and updated build script

---
+++ src/build.bat
@@ -0,0 +1,17 @@
+@echo off
+set ZIP=C:\PROGRA~1\7-Zip\7z.exe a -tzip -y -r
+set REPO=hoochie_baby
+set PACKID=1847358755
+set VERSION=0.0.7
+
+
+quick_manifest.exe "Hoochie Baby: QCtrl DLrnQ" "%PACKID%" >%REPO%\manifest.json
+echo %VERSION% >%REPO%\VERSION
+
+fsum -r -jm -md5 -d%REPO% * > checksum.md5
+move checksum.md5 %REPO%\checksum.md5
+
+%ZIP% %REPO%_v%VERSION%_Anki20.zip *.py %REPO%\*
+cd %REPO%
+%ZIP% ../%REPO%_v%VERSION%_Anki21.ankiaddon *
+%ZIP% ../%REPO%_v%VERSION%_CCBC.adze *
