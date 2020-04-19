73e2973e465046dc67bdfce895c540643c1367ff
lovac42 <lovac42@protonmail.com>
2020-04-18 22:14:52 -0400

    Updated version warning to safety_first module.

--- src/hoochie_baby/const.py
+++ src/hoochie_baby/const.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 # Copyright: (C) 2018-2020 Lovac42
-# Support: https://github.com/lovac42/HoochieMama
+# Support: https://github.com/lovac42/HoochieBaby
 # License: GNU GPL, version 3 or later; http://www.gnu.org/copyleft/gpl.html
 
+
+import os
+
+ADDON_PATH = os.path.dirname(__file__)
 
 ADDON_NAME = "HoochieBaby"
 
