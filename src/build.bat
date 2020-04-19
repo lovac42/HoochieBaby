b942b9daab00e90665bce8f171001abd57d5ad7a
lovac42 <lovac42@protonmail.com>
2020-04-19 00:34:22 -0400

    bump version to v0.2.0

--- src/build.bat
+++ src/build.bat
@@ -2,16 +2,20 @@
 set ZIP=C:\PROGRA~1\7-Zip\7z.exe a -tzip -y -r
 set REPO=hoochie_baby
 set PACKID=1847358755
-set VERSION=0.1.1
+set VERSION=0.2.0
 
 
-quick_manifest.exe "Hoochie Baby: QCtrl DLrnQ" "%PACKID%" >%REPO%\manifest.json
 echo %VERSION% >%REPO%\VERSION
 
 fsum -r -jm -md5 -d%REPO% * > checksum.md5
 move checksum.md5 %REPO%\checksum.md5
 
 %ZIP% %REPO%_v%VERSION%_Anki20.zip *.py %REPO%\*
+
 cd %REPO%
+
+quick_manifest.exe "Hoochie Baby: QCtrl DLrnQ" "%PACKID%" >%REPO%\manifest.json
 %ZIP% ../%REPO%_v%VERSION%_Anki21.ankiaddon *
+
+quick_manifest.exe "Hoochie Baby: QCtrl DLrnQ" "%REPO%" >%REPO%\manifest.json
 %ZIP% ../%REPO%_v%VERSION%_CCBC.adze *
