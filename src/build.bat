d86a4621007a5b488423226e86be9cf7fe113a10
lovac42 <lovac42@protonmail.com>
2020-04-20 20:54:40 -0400

    Added config to disable self test for slow machines

--- src/build.bat
+++ src/build.bat
@@ -2,7 +2,7 @@
 set ZIP=C:\PROGRA~1\7-Zip\7z.exe a -tzip -y -r
 set REPO=hoochie_baby
 set PACKID=1847358755
-set VERSION=0.2.0
+set VERSION=0.2.1
 
 
 echo %VERSION% >%REPO%\VERSION
@@ -14,8 +14,8 @@ move checksum.md5 %REPO%\checksum.md5
 
 cd %REPO%
 
-quick_manifest.exe "Hoochie Baby: QCtrl DLrnQ" "%PACKID%" >%REPO%\manifest.json
+quick_manifest.exe "Hoochie Baby: QCtrl DLrnQ" "%PACKID%" >manifest.json
 %ZIP% ../%REPO%_v%VERSION%_Anki21.ankiaddon *
 
-quick_manifest.exe "Hoochie Baby: QCtrl DLrnQ" "%REPO%" >%REPO%\manifest.json
+quick_manifest.exe "Hoochie Baby: QCtrl DLrnQ" "%REPO%" >manifest.json
 %ZIP% ../%REPO%_v%VERSION%_CCBC.adze *
--- src/hoochie_baby/self_test.py
+++ src/hoochie_baby/self_test.py
@@ -7,6 +7,8 @@
 from aqt import mw
 from aqt.utils import tooltip
 
+from .const import ADDON_NAME
+from .config import Config
 from .lib.com.lovac42.anki.backend.utils import isSorted
 from .lib.com.lovac42.anki.version import ANKI20
 
@@ -15,6 +17,8 @@ if ANKI20:
 
 
 class Tests:
+    conf = Config(ADDON_NAME)
+
     def __init__(self):
         self.reset()
 
@@ -41,7 +45,7 @@ class Tests:
 
     def testWrap(self, checkbox):
         "This tests for addon conflict, to make sure HoochieBaby was wrapped correctly around fillLrnDay()."
-        if self.isReview():
+        if not self.conf.get("run_self_test", True) or self.isReview():
             return
 
         self.reset()
@@ -72,9 +76,8 @@ class Tests:
             raise ValueError("Checkbox state was not expected.")
 
 
-
     def testSort(self, index):
-        if self.isReview():
+        if not self.conf.get("run_self_test", True) or self.isReview():
             return
 
         expected=0
